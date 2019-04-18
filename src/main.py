76d6b63ba8f1093a9a9abc3d69c4ec9eed11550d
Vaibhav Tulsyan <vstulsyan@gmail.com>
2019-04-18 16:04:26 +0530

    Add seq2seq code

---
+++ src/main.py
@@ -0,0 +1,115 @@
+from __future__ import print_function
+
+from keras.models import Model
+from keras.layers import Input, LSTM, Dense
+import numpy as np
+
+batch_size = 64  # Batch size for training.
+epochs = 100  # Number of epochs to train for.
+latent_dim = 256  # Latent dimensionality of the encoding space.
+num_samples = 10000  # Number of samples to train on.
+# Path to the data txt file on disk.
+data_path = 'fra.txt'
+
+# Vectorize the data.
+input_texts = []
+target_texts = []
+input_words = set()
+target_words = set()
+
+# File format: <eng-word>\t<french-word>\n
+# Get all lines from file
+with open(data_path, 'r', encoding='utf-8') as f:
+    lines = f.read().split('\n')
+
+input_sequences = []
+output_sequences = []
+# Go through number of samples you want to train on
+for line in lines[: min(num_samples, len(lines) - 1)]:
+    input_text, target_text = line.split('\t')
+    # We use "tab" as the "start sequence" character
+    # for the targets, and "\n" as "end sequence" character.
+    target_text = '\t' + target_text + '\n'
+    input_texts.append(input_text)
+    target_texts.append(target_text)
+    input_token_list = input_text.split()
+    output_token_list = target_text.split()
+    input_sequences.append(input_token_list)
+    output_sequences.append(output_token_list)
+    for word in input_token_list:
+        if word not in input_words:
+            input_words.add(word)
+    for word in output_token_list:
+        if word not in target_words:
+            target_words.add(word)
+
+input_words = sorted(list(input_words))
+target_words = sorted(list(target_words))
+num_encoder_tokens = len(input_words)
+num_decoder_tokens = len(target_words)
+max_encoder_seq_length = max([len(seq) for seq in input_sequences])
+max_decoder_seq_length = max([len(seq) for seq in output_sequences])
+
+print('Number of samples:', len(input_texts))
+print('Number of unique input tokens:', num_encoder_tokens)
+print('Number of unique output tokens:', num_decoder_tokens)
+print('Max sequence length for inputs:', max_encoder_seq_length)
+print('Max sequence length for outputs:', max_decoder_seq_length)
+
+
+input_token_index = dict(
+    [(word, i) for i, word in enumerate(input_words)])
+target_token_index = dict(
+    [(word, i) for i, word in enumerate(target_words)])
+
+encoder_input_data = np.zeros(
+    (len(input_texts), max_encoder_seq_length, num_encoder_tokens),
+    dtype='float32')
+decoder_input_data = np.zeros(
+    (len(input_texts), max_decoder_seq_length, num_decoder_tokens),
+    dtype='float32')
+decoder_target_data = np.zeros(
+    (len(input_texts), max_decoder_seq_length, num_decoder_tokens),
+    dtype='float32')
+
+for i, (input_sequences, output_sequences) in enumerate(zip(input_sequences, output_sequences)):
+    for t, word in enumerate(input_sequences):
+        encoder_input_data[i, t, input_token_index[word]] = 1.
+    for t, word in enumerate(output_sequences):
+        # decoder_target_data is ahead of decoder_input_data by one timestep
+        decoder_input_data[i, t, target_token_index[word]] = 1.
+        if t > 0:
+            # decoder_target_data will be ahead by one timestep
+            # and will not include the start character.
+            decoder_target_data[i, t - 1, target_token_index[word]] = 1.
+
+# Define an input sequence and process it.
+encoder_inputs = Input(shape=(None, num_encoder_tokens))
+encoder = LSTM(latent_dim, return_state=True)
+encoder_outputs, state_h, state_c = encoder(encoder_inputs)
+# We discard `encoder_outputs` and only keep the states.
+encoder_states = [state_h, state_c]
+
+# Set up the decoder, using `encoder_states` as initial state.
+decoder_inputs = Input(shape=(None, num_decoder_tokens))
+# We set up our decoder to return full output sequences,
+# and to return internal states as well. We don't use the
+# return states in the training model, but we will use them in inference.
+decoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True)
+decoder_outputs, _, _ = decoder_lstm(decoder_inputs,
+                                     initial_state=encoder_states)
+decoder_dense = Dense(num_decoder_tokens, activation='softmax')
+decoder_outputs = decoder_dense(decoder_outputs)
+
+# Define the model that will turn
+# `encoder_input_data` & `decoder_input_data` into `decoder_target_data`
+model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+
+# Run training
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+model.fit([encoder_input_data, decoder_input_data], decoder_target_data,
+          batch_size=batch_size,
+          epochs=epochs,
+          validation_split=0.2)
+# Save model
+model.save('s2s.h5')
