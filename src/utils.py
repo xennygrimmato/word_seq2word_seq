c807c93fc182acc461def5c87ec97efae3efcadd
Lucky Kadam <luckykadam94@gmail.com>
2019-04-28 15:43:14 +0530

    use keras preprocsssion for padding

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,8 @@
 import numpy as np
+from keras.utils import to_categorical
+from keras.preprocessing.sequence import pad_sequences
 
 
-def one_hot_encode(seqs, length, num_tokens):
-    data = np.zeros(
-        (len(seqs), length, num_tokens),
-        dtype='float32'
-    )
-    for i, seq in enumerate(seqs):
-        for t, token in enumerate(seq):
-            data[i, t, token] = 1.
-    return data
+def one_hot_encode(seqs, length, num_tokens, padding='post'):
+    one_hot_seqs = [to_categorical(seq, num_classes=num_tokens) for seq in seqs]
+    return pad_sequences(one_hot_seqs, maxlen=length, dtype='float32', padding=padding, value=np.zeros(shape=(num_tokens,)))
