944887a590e5913ca31e9815d2329736723b4b6c
Lucky Kadam <luckykadam94@gmail.com>
2019-04-28 17:28:11 +0530

    update model to take integer sequence (indices) and generate one hot encoding on the fly using embedding layer

--- src/seq2seq.py
+++ src/seq2seq.py
@@ -1,13 +1,13 @@
 from __future__ import print_function
 
 from keras.models import Model
-from keras.layers import Input, LSTM, Dense
+from keras.layers import Input, LSTM, Dense, Embedding
 import numpy as np
 
-from src.utils import one_hot_encode
+from src.utils import one_hot_encode, add_one
 
 
-class Seq2SeqModel:
+class Seq2Seq:
     def __init__(self, num_encoder_tokens, num_decoder_tokens, start_token, end_token, latent_dim=256):
         self.num_encoder_tokens = num_encoder_tokens
         self.num_decoder_tokens = num_decoder_tokens
@@ -16,19 +16,31 @@ class Seq2SeqModel:
         self.end_token = end_token
 
         # Define an input sequence and process it.
-        encoder_inputs = Input(shape=(None, self.num_encoder_tokens))
+        encoder_inputs = Input(shape=(None,))
+        encoder_emb = Embedding(
+            num_encoder_tokens+1,
+            num_encoder_tokens,
+            weights=[np.concatenate((np.zeros(shape=(1, num_encoder_tokens)), np.identity(num_encoder_tokens)))],
+            trainable=False
+        )(encoder_inputs)
         encoder = LSTM(latent_dim, return_state=True)
-        encoder_outputs, state_h, state_c = encoder(encoder_inputs)
+        encoder_outputs, state_h, state_c = encoder(encoder_emb)
         # We discard `encoder_outputs` and only keep the states.
         encoder_states = [state_h, state_c]
 
         # Set up the decoder, using `encoder_states` as initial state.
-        decoder_inputs = Input(shape=(None, self.num_decoder_tokens))
+        decoder_inputs = Input(shape=(None,))
+        decoder_emb = Embedding(
+            num_decoder_tokens+1,
+            num_decoder_tokens,
+            weights=[np.concatenate((np.zeros(shape=(1, num_decoder_tokens)), np.identity(num_decoder_tokens)))],
+            trainable=False
+        )(decoder_inputs)
         # We set up our decoder to return full output sequences,
         # and to return internal states as well. We don't use the
         # return states in the training model, but we will use them in inference.
         decoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True)
-        decoder_outputs, _, _ = decoder_lstm(decoder_inputs,
+        decoder_outputs, _, _ = decoder_lstm(decoder_emb,
                                              initial_state=encoder_states)
         decoder_dense = Dense(self.num_decoder_tokens, activation='softmax')
         decoder_outputs = decoder_dense(decoder_outputs)
@@ -44,7 +56,7 @@ class Seq2SeqModel:
         decoder_state_input_c = Input(shape=(latent_dim,))
         decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
         decoder_outputs, state_h, state_c = decoder_lstm(
-            decoder_inputs, initial_state=decoder_states_inputs)
+            decoder_emb, initial_state=decoder_states_inputs)
         decoder_states = [state_h, state_c]
         decoder_outputs = decoder_dense(decoder_outputs)
         self.decoder_model = Model(
@@ -52,10 +64,16 @@ class Seq2SeqModel:
             [decoder_outputs] + decoder_states)
 
     def pre_process_encoder_input(self, input):
-        return one_hot_encode(input, self.max_encoder_seq_length, self.num_encoder_tokens)
+        # return one_hot_encode(input, self.max_encoder_seq_length, self.num_encoder_tokens)
+        return add_one(input, self.max_encoder_seq_length)
 
     def pre_process_decoder_input(self, input):
+        # return one_hot_encode(input, self.max_decoder_seq_length, self.num_decoder_tokens)
+        return add_one(input, self.max_decoder_seq_length)
+
+    def pre_process_decoder_target(self, input):
         return one_hot_encode(input, self.max_decoder_seq_length, self.num_decoder_tokens)
+        # return add_one(input, self.max_decoder_seq_length)
 
     def train(self, encoder_input_seqs, decoder_input_seqs, decoder_target_seqs, batch_size=64, epochs=100):
         # Run training
@@ -63,7 +81,10 @@ class Seq2SeqModel:
         self.max_decoder_seq_length = max([len(seq) for seq in decoder_input_seqs])
         encoder_input_data = self.pre_process_encoder_input(encoder_input_seqs)
         decoder_input_data = self.pre_process_decoder_input(decoder_input_seqs)
-        decoder_target_data = self.pre_process_decoder_input(decoder_target_seqs)
+        decoder_target_data = self.pre_process_decoder_target(decoder_target_seqs)
+        print('encoder_input_data.shape: ', encoder_input_data.shape)
+        print('decoder_input_data.shape: ', decoder_input_data.shape)
+        print('decoder_target_data.shape: ', decoder_target_data.shape)
         self.train_model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
         self.train_model.fit([encoder_input_data, decoder_input_data], decoder_target_data,
                   batch_size=batch_size,
@@ -81,10 +102,11 @@ class Seq2SeqModel:
 
     def decode(self, states_value):
         # Generate empty target sequence of length 1.
-        target_seq = np.zeros((1, 1, self.num_decoder_tokens))
+        # target_seq = np.zeros((1, 1, self.num_decoder_tokens))
         # Populate the first token of target sequence with the start token.
-        target_seq[0, 0, self.start_token] = 1.
-
+        # target_seq[0, 0, self.start_token] = 1.
+        target_seq = np.zeros((1, 1))
+        target_seq[0, 0] = self.start_token + 1
         # Sampling loop for a batch of sequences
         # (to simplify, here we assume a batch of size 1).
         stop_condition = False
@@ -104,8 +126,10 @@ class Seq2SeqModel:
                 stop_condition = True
 
             # Update the target sequence (of length 1).
-            target_seq = np.zeros((1, 1, self.num_decoder_tokens))
-            target_seq[0, 0, sampled_token_index] = 1.
+            # target_seq = np.zeros((1, 1, self.num_decoder_tokens))
+            # target_seq[0, 0, sampled_token_index] = 1.
+            target_seq = np.zeros((1, 1))
+            target_seq[0, 0] = sampled_token_index + 1
 
             # Update states
             states_value = [h, c]
--- src/utils.py
+++ src/utils.py
@@ -6,3 +6,8 @@ from keras.preprocessing.sequence import pad_sequences
 def one_hot_encode(seqs, length, num_tokens, padding='post'):
     one_hot_seqs = [to_categorical(seq, num_classes=num_tokens) for seq in seqs]
     return pad_sequences(one_hot_seqs, maxlen=length, dtype='float32', padding=padding, value=np.zeros(shape=(num_tokens,)))
+
+
+def add_one(seqs, length, padding='post'):
+    new_seqs = [[e+1 for e in seq] for seq in seqs]
+    return pad_sequences(new_seqs, maxlen=length, padding=padding)
